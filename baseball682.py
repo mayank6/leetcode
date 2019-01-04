ea3144f7a43cce808c556b138219e9d735c131b3
mayank6 <chauhanmayank.621@gmail.com>
2019-01-04 12:25:08 +0530

    Create baseball682.py

---
+++ baseball682.py
@@ -0,0 +1,12 @@
+ops= ["5","-2","4","C","D","9","+","+"]
+stack = []
+for op in ops:
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif op == 'C':
+                stack.pop()
+            elif op == 'D':
+                stack.append(2 * stack[-1])
+            else:
+                stack.append(int(op))
+print(sum(stack))
