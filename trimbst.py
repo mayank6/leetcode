171c05ab76ea9296f068050bfbd05f9f5e6c22be
mayank6 <chauhanmayank.621@gmail.com>
2019-01-05 16:26:52 +0530

    Create trimbst.py

---
+++ trimbst.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def trimBST(self, root, L, R):
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+
+        return trim(root)
