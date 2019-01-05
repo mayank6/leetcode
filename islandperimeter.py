dc97b56a557d028f5db53f8363400dfeff219e7a
mayank6 <chauhanmayank.621@gmail.com>
2019-01-05 11:25:04 +0530

    Update islandperimeter.py

--- islandperimeter.py
+++ islandperimeter.py
@@ -21,3 +21,26 @@ class Solution:
                         if grid[i][j] == 1:
                             count += sum_adjacent(i, j)
         return count
+
+    #-----------------------------------------------------------------------#
+    class Solution:
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        ans=0
+        row,col=len(grid),len(grid[0])
+        for i in range(row):
+            for j in range(col):
+               if grid[i][j]==1:
+                ans+=4
+                if i-1>=0 and grid[i-1][j]==1:
+                    ans-=1
+                if i+1<row and grid[i+1][j]==1:
+                    ans-=1
+                if j+1<col and grid[i][j+1]==1:
+                    ans-=1
+                if j-1>=0 and grid[i][j-1]==1:
+                    ans-=1
+        return ans
