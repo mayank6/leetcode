297f49e89cdcdb3a6a90a3ce01563695dc990768
mayank6 <chauhanmayank.621@gmail.com>
2019-01-07 19:53:51 +0530

    Create singlenumber136.py

---
+++ singlenumber136.py
@@ -0,0 +1,9 @@
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
