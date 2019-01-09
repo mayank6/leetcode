f6f3e855e5615846718386637f1a86656c4a18fe
mayank6 <chauhanmayank.621@gmail.com>
2019-01-09 12:36:40 +0530

    Create avgleveltree637.py

---
+++ avgleveltree637.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+        if root is None:
+            return []
+        i=0
+        q=[root,]
+        k=1
+        ans=[]
+        avg=0.0
+        while(q):
+            while(i<k):
+                root=q.pop(0)
+                i+=1
+                if root:
+                    avg+=root.val
+                    
+                    if root.left and root.right:
+                        q.append(root.left)
+                        q.append(root.right)
+                    elif root.left:
+                        q.append(root.left)
+                    elif root.right:
+                        q.append(root.right)
+                    else:
+                        pass
+            print(avg,k)
+            avg=avg/k           
+            ans.append(avg)
+            avg=0.0
+            k=len(q)
+            i=0
+        return ans
