e39ec3e8d86f92b30095e89831c171de7ac238a1
mayank6 <chauhanmayank.621@gmail.com>
2019-01-11 06:36:00 +0530

    Create monotonicarray.py

---
+++ monotonicarray.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isMonotonic(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        p=0
+        n=0
+        z=1
+        for i in range(len(A)-1):
+            x=A[i]-A[i+1]
+            if x>0:
+                p=1
+            elif x<0:
+                n=1
+            else:
+                z+=1
+        if len(A)==z:
+            return True
+        elif p!=n:
+            return True
+        else:
+            return False
+        
+            
+            
