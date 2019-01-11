96bc5af3c68e84c1002261025c8fa08b32c53f3c
mayank6 <chauhanmayank.621@gmail.com>
2019-01-11 06:16:29 +0530

    Update and rename permutationletter.py to $permutationletter.py

---
+++ $permutationletter.py
@@ -0,0 +1,42 @@
+S = "3z4abc"
+def letterCasePermutation(S):
+    ans = []
+ 
+    def dfs(S, i, n):
+      if i == n:
+        ans.append(''.join(S))
+        return
+      
+      dfs(S, i + 1, n)      
+      if not S[i].isalpha(): return      
+      S[i] = chr(ord(S[i]) ^ (1<<5))
+      dfs(S, i + 1, n)
+      S[i] = chr(ord(S[i]) ^ (1<<5)) #backtracking
+    
+    dfs(list(S), 0, len(S))
+    return ans
+print(letterCasePermutation(S))
+
+#-----------------------------------------#
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        q=[]
+        q.append("")
+        level=-1
+        while True:
+            level+=1
+            if level==len(S):
+                break
+            for i in range(len(q)):
+                ch=S[level]
+                element=q.pop(0)
+                if ch.isdigit():
+                    q.append("{}{}".format(element,ch))
+                else:
+                    q.append("{}{}".format(element,ch.lower()))
+                    q.append("{}{}".format(element,ch.upper()))
+        return q
