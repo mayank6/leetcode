8e36293c2a990005b995d762778f8a6a1c0dbd77
Himanshu sharma <himanshush214@gmail.com>
2019-10-05 13:09:48 +0530

    ADDED MY SOLUTION

--- movezero.py
+++ movezero.py
@@ -12,3 +12,14 @@ class Solution(object):
         while i1<len(nums):
             nums[i1]=0
             i1+=1
+#SOLUTION By HIMANSHU
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if i==0:
+                s = nums.pop(nums.index(i))
+                nums.append(s)
